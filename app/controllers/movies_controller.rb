4be8b7f391a946e04a5575e46a0490c4e113631f
Joe Ray <jray100.jr@gmail.com>
2021-06-01 10:19:08 -0600

    Add form to view and conditional to movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,9 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @movie_actors = @movie.actors
+    
+    if params[:name]
+      @movie_actors << Actor.find_actor(params[:name])
+    end
   end
 end
