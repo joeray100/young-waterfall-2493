30a9d0b11332a2c4701ff49549d4941a82fc9bfb
Joe Ray <jray100.jr@gmail.com>
2021-06-01 10:34:28 -0600

    Add actor method worked_with

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,15 @@ class Actor < ApplicationRecord
   def self.find_actor(actor_name)
     where('name ILIKE ?', "%#{actor_name}%")
   end
+
+  # I know this is not correct as it is not dynamic enough to handle different conditions, would love feedback on the correct way of doing it?
+  # Makes sense to start with class method so you can start with all of the actors and widdle it down using maybe the movies they are attached to somehow and where.not the actor themselves?
+  # you can pull the actor in as argument from controller using -> @actor = Actor.find(params[:id])
+  # @worked_with = Actor.worked_with(@actor)
+  def self.worked_with
+    joins(:movies)
+    .where(movies: {title: 'Cruella'})
+    .where.not(name: 'Emma Stone')
+    .uniq
+  end
 end
