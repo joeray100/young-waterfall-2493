a302e2c45e648563c484d2869cbc40f91bdf58f0
Joe Ray <jray100.jr@gmail.com>
2021-06-01 09:52:20 -0600

    Add relationships and studio_actors method

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,9 @@
 class Studio < ApplicationRecord
   has_many :movies
+  has_many :movie_actors, through: :movies
+  has_many :actors, through: :movie_actors
+
+  def studio_actors
+    actors.where(currently_working: true).order(:age).distinct
+  end
 end
