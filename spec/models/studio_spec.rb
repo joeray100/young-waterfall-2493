ca8c86789921357453ac45ee3a83952db253ef4f
Joe Ray <jray100.jr@gmail.com>
2021-06-01 10:50:18 -0600

    Add relationship tests for studio

--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe Studio do
   describe 'relationships' do
     it {should have_many :movies}
+    it {should have_many(:movie_actors).through(:movies)}
+    it {should have_many(:actors).through(:movie_actors)}
   end
 
   before :each do
